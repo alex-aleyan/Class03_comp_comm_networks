ac52c16b4b48e27efbded969d3e07442fcc5feab
Alex Aleyan <sasha.aleyan@gmail.com>
2019-10-02 12:52:46 -0400

    clean up the read file line by line example

--- project/playground/read_file_linebyline/README.txt
+++ project/playground/read_file_linebyline/README.txt
@@ -1,1 +1,1 @@
-
+./readfile.run
--- project/playground/read_file_linebyline/readfile.c
+++ project/playground/read_file_linebyline/readfile.c
@@ -10,45 +10,33 @@
 #include <stdlib.h>
 
 
+
+
 int main (int argc, char **argv)
 {
 				
 				FILE *fp;
-				char current_line [255];
+
+				fp = fopen("file.txt", "r");
+
+
+
 
 				int line_count = 0;
 				int char_count = 0;
 				char current_char;
-    
-				fp = fopen("file.txt", "r");
 
-//				for (current_char = getc(fp); current_char != EOF; current_char = getc(fp))	
-//				{
-//								char_count++;
-//								if (current_char == '\n') 
-//								{
-//												printf("Number of char per line %d: %d\n", line_count, char_count);
-//												char_count = 0;
-//												line_count++;
-//								}
-//				}
-//				printf("\nNumber of lines: %d\n\n", line_count);
-
-
+				//count the number of lines in file to determine the size of the pointer array:
 				for (current_char = getc(fp); current_char != EOF; current_char = getc(fp))	
-				{
-								if (current_char == '\n') 
-								{
-												line_count++;
-								}
-				}
+								if (current_char == '\n') line_count++;
+
 				printf("\nNumber of lines: %d\n\n", line_count);
 
 				//allocate an array of pointers (1 pointer per line of text in file):
 				char * line_ptr[line_count];
 
-				rewind(fp);
 				// determine number of chars per each line and allocate just enough memory per each line of text:
+				rewind(fp);
 				for (current_char = getc(fp), line_count=0; current_char != EOF; current_char = getc(fp))	
 				{
 								char_count++;
@@ -63,15 +51,35 @@ int main (int argc, char **argv)
 				}
 
 				printf("\n");
+
 				rewind(fp);
 
-				//copy file content to allocated memory line by line:
+				//line_count=0;
+				//char current_line [255];
+				//while( fgets(current_line, 255, (FILE*) fp) )
+				//{
+				//				strcpy(line_ptr[line_count], current_line);
+				//				printf("%s", line_ptr[line_count]);
+				//				line_count++;
+			 //}
+			 
+				//copy file content to allocated memory line by line
+				//this is slower than the commented out section above since reading
+				//one character at the time but we don't have to allocate additional 255
+				//bytes of data
 				line_count=0;
-				while( fgets(current_line, 255, (FILE*) fp) )
+				char_count=0;
+				for (current_char = getc(fp); current_char != EOF; current_char = getc(fp))	
 				{
-								strcpy(line_ptr[line_count], current_line);
-								printf("%s", line_ptr[line_count]);
-								line_count++;
+								*(line_ptr[line_count]+char_count)=current_char;
+								char_count++;
+								if (current_char == '\n') 
+								{
+												printf("Line(%d): ", line_count);
+												printf("%s", line_ptr[line_count]);
+												char_count=0;
+												line_count++;
+								}
 				}
 
 				fclose(fp);
